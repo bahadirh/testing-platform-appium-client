b1d1365685d008b6d5bd71a4d2717231db2f131a
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-17 12:56:51 +0300

    attach functions to platforms fn

--- utils/platforms.js
+++ utils/platforms.js
@@ -1,12 +1,17 @@
-module.exports.onPlatform = client => {
+module.exports.onPlatform = (client, functions) => {
   const platform = client.capabilities.platformName
   const fn = async (actions, ...args) => {
     return await actions[platform](client, ...args)
   }
+  // TODO: move wait for exist outside
   fn.waitForExist = async (element, ms = 3000) => {
     await element.waitForExist(ms)
     return fn
   }
+  // assigns functions to the return object as keys
+  Object.keys(functions).forEach(helper => {
+    fn[helper] = functions[helper]
+  })
 
   return fn
 }
