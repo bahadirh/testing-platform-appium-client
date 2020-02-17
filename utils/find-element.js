e87c3818f97b65d876714fe59e876d4761e9f0bb
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-17 11:33:44 +0300

    add todo comments to findElement ios fn

--- utils/find-element.js
+++ utils/find-element.js
@@ -3,7 +3,9 @@ const findElementOnAndroid = async (client, selector, value) => {
   return await client.$(`android=new UiSelector().${selector}("${value}")`)
 }
 
-const findElementOnIOS = () => {}
+const findElementOnIOS = () => {
+  // TODO: implement it
+}
 
 module.exports = {
   Android: findElementOnAndroid,
