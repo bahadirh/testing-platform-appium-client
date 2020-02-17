1d6114ad1016f48b733a3d8b0d8ee7218913b4e5
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-17 11:33:44 +0300

    add todo comments to findElement ios fn

--- utils/platforms.js
+++ utils/platforms.js
@@ -1,3 +1,12 @@
-module.exports.onPlatform = client => async (actions, ...args) => {
-  return await actions[client.capabilities.platformName](client, ...args)
+module.exports.onPlatform = client => {
+  const platform = client.capabilities.platformName
+  const fn = async (actions, ...args) => {
+    return await actions[platform](client, ...args)
+  }
+  fn.waitForExist = async (element, ms = 3000) => {
+    await element.waitForExist(ms)
+    return fn
+  }
+
+  return fn
 }
