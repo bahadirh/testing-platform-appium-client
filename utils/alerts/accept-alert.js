de8739b630ae4fb97cac58cc7add5da70f93684d
Bahadır Hocamoğlu <bahadirh@Bahobaho-mac.local>
2020-02-21 17:06:41 +0300

    add template functions for alert handling

---
+++ utils/alerts/accept-alert.js
@@ -0,0 +1,20 @@
+const dismissAlertOnAndroid = async (client, ignoreErrors = true) => {
+  try {
+    return await client.dismissAlert()
+  } catch (error) {
+    if (!ignoreErrors) {
+      throw error
+    } else {
+      return error
+    }
+  }
+}
+
+const dismissAlertOnIOS = async (client, ingoreErrors = true) => {
+  // TODO: implement dismissing alert on iOS
+}
+
+module.exports = {
+  Android: dismissAlertOnAndroid,
+  iOS: dismissAlertOnIOS
+}
