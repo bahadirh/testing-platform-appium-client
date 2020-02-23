076d5a1cb340058f71d2d24df2ac807e5bfc794b
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-23 18:33:46 +0300

    implement acceptAlert for ios

--- utils/alerts/accept-alert.js
+++ utils/alerts/accept-alert.js
@@ -10,8 +10,16 @@ const acceptAlertOnAndroid = async (client, ignoreErrors = true) => {
   }
 }
 
-const acceptAlertOnIOS = async (client, ingoreErrors = true) => {
-  // TODO: implement dismissing alert on iOS
+const acceptAlertOnIOS = async (client, ignoreErrors = true) => {
+  try {
+    return await client.acceptAlert()
+  } catch (error) {
+    if (!ignoreErrors) {
+      throw error
+    } else {
+      return error
+    }
+  }
 }
 
 module.exports = {
