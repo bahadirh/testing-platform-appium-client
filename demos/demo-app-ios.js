ce90217fc3dcb9f65c772d2fc2918869d88e7f67
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-23 17:25:33 +0300

    add sample demo app test for ios

---
+++ demos/demo-app-ios.js
@@ -0,0 +1,40 @@
+const wdio = require('webdriverio')
+
+const utils = require('../utils/v2')
+
+const opts = {
+  logLevel: 'silent',
+  port: 4723,
+  capabilities: {
+    // acceptInsecureCerts: true,
+    // allowTestPackages: true,
+    app: `/Users/bahadirh/Library/Developer/Xcode/DerivedData/AppiumTest-hbnatelqufgnxpddfjqsmwplhzjd/Build/Products/Debug-iphonesimulator/AppiumTest.app`,
+    // app: `${__dirname}/../ipa/iOS-Simulator-NativeDemoApp-0.2.1.app`,
+    // appActivity: 'main.MainActivity',
+    // appPackage: 'baho.HelloWorld',
+    automationName: 'XCUITest',
+    // bundleId: 'baho.HelloWorld',
+    deviceName: 'iPhone 11 Pro Max',
+    platformName: 'iOS',
+    platformVersion: '13.3'
+  }
+}
+
+async function main() {
+  await utils.actionSequence(null, async () => {
+    let element
+    const client = await wdio.remote({ ...opts })
+    const _ = utils.platforms.onPlatform(client, utils)
+
+    await _(_.actionSequence, async () => {
+      //   element = await client.$(`XCUIElementTypeTextField`)
+      element = await _(_.findElement, 'name', 'emailTextField')
+
+      await element.setValue('ba@ho.com')
+    })
+
+    await client.deleteSession()
+  })
+}
+
+main()
