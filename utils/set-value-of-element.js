ae3f80d14e83cd41e4be45be59dbbfc491ef5bd1
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-24 12:02:26 +0300

    remove redundant files and todo tags

--- utils/set-value-of-element.js
+++ utils/set-value-of-element.js
@@ -3,7 +3,6 @@ const setValueOnAndroid = async (client, element, data) => {
 }
 
 const setValueOnIOS = async (client, element, data) => {
-  // TODO: implement setting given value to the element given on iOS
   return await element.setValue(data)
 }
 
