8fb95937b927d0634b7f39b649034609bc2dd8d1
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-02-24 12:05:40 +0300

    replace waitForElement with waitForExist

--- utils/v2.js
+++ utils/v2.js
@@ -1,6 +1,5 @@
 const platforms = require('./platforms')
 const findElement = require('./find-element')
-const waitForElement = require('./wait-for-element')
 const clickElement = require('./click-element')
 const pushFile = require('./push-file')
 const saveScreenshot = require('./save-screenshot')
@@ -19,7 +18,6 @@ module.exports = {
   pushFile,
   saveScreenshot,
   setValue,
-  waitForElement,
   waitForExist,
   ...alerts
 }
