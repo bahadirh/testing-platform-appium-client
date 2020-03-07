34d39392da230ed1494dbb66fd9daabbdaf0903a
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-03-07 22:14:39 +0300

    remove files of old version of utils

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,25 @@
-const elements = require('./elements')
+const platforms = require('./platforms')
+const findElement = require('./find-element')
+const clickElement = require('./click-element')
+const pushFile = require('./push-file')
+const saveScreenshot = require('./save-screenshot')
+const actionSequence = require('./action-sequence')
+const setValue = require('./set-value-of-element')
+const findElementByXpath = require('./find-element-by-xpath')
+const alerts = require('./alerts')
+const waitForExist = require('./wait-for-exist')
+const getPageSource = require('./get-page-source')
 
 module.exports = {
-  ...elements
+  actionSequence,
+  clickElement,
+  findElement,
+  findElementByXpath,
+  getPageSource,
+  platforms,
+  pushFile,
+  saveScreenshot,
+  setValue,
+  waitForExist,
+  ...alerts
 }
