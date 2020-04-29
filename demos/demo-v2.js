39f09df5740c5ce0000fabb9a2f456c68f251a9c
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2020-04-29 13:16:01 +0300

    Update android demo-v2

--- demos/demo-v2.js
+++ demos/demo-v2.js
@@ -5,28 +5,17 @@ const opts = {
   port: 4723,
   capabilities: {
     platformName: 'Android',
-    platformVersion: '9',
+    platformVersion: '10',
     deviceName: 'Android Emulator',
-    app: `${__dirname}/apk/ApiDemos-debug.apk`,
+    app: `${__dirname}/../apk/ApiDemos-debug.apk`,
     appPackage: 'io.appium.android.apis',
-    // appActivity: '.view.TextFields',
-    automationName: 'UiAutomator2'
-  }
+    appActivity: '.view.TextFields',
+    automationName: 'UiAutomator2',
+  },
 }
 
 async function main() {
-  const client = await wdio.remote(opts)
-
-  const viewsWithSelector = await client.$(
-    `android=new UiSelector().text("Text")`
-  )
-  await viewsWithSelector.click()
-
-  const views = await client.$('~Views')
-  await views.click()
-
-  const textFields = await client.$('android.widget.TextView')
-  await textFields.click()
+  const client = await wdio.remote({ ...opts })
 
   const field = await client.$('android.widget.EditText')
   await field.setValue('Hello World!')
